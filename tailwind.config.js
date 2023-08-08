afcc794175b1fb4beec443423ac89606f3e8756f
El-hag-youssef <140336295+El-hag-youssef@users.noreply.github.com>
2023-08-08 23:40:08 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,html}',
+    './public/**/*.{js,jsx,html}',
+    './*.{js,jsx,html}',
+
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
